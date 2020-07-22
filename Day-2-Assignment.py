d8e035d210205dc9ef290ab48c1f582b15036df4
teju0809 <46315769+teju0809@users.noreply.github.com>
2020-07-23 00:05:36 +0530

    Add files via upload

---
+++ Day-2-Assignment.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello world")
+
+
+# In[3]:
+
+
+print("python's world")
+
+
+# In[4]:
+
+
+print("python\n world")
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+print("python \t world")
+
+
+# In[7]:
+
+
+print('python\'s world')
+
+
+# In[10]:
+
+
+name= "john"
+marks=20
+age=26
+print("My name is %s I have %f marks and my age is %d"%(name,marks,age) )
+
+
+# In[15]:
+
+
+name= "john"
+marks=20
+age=26
+print("My name is %s I have %.2f marks and my age is %d"%(name,marks,age) )
+
+Variables
+# In[17]:
+
+
+_=10
+
+
+# 
+
+# In[18]:
+
+
+_
+
+
+# In[19]:
+
+
+id(_)    #Ram address of variable
+
+
+# #Arithmatic operators
+
+# In[20]:
+
+
+5**9
+
+
+# In[21]:
+
+
+5**2 #power of **
+
+
+# In[22]:
+
+
+5*2
+
+
+# In[23]:
+
+
+89+56
+
+
+# In[24]:
+
+
+78-89
+
+
+# In[25]:
+
+
+89/9
+
+
+# In[26]:
+
+
+15/5
+
+
+# In[27]:
+
+
+10%3
+
+
+# In[29]:
+
+
+10/3
+
+
+# In[30]:
+
+
+10//3 #Floor 
+
+
+# Logical Operators 
+
+# In[31]:
+
+
+a=10
+b=18
+a==b
+
+
+# In[32]:
+
+
+a!=b
+
+
+# In[33]:
+
+
+a>b
+
+
+# In[35]:
+
+
+a<b
+
+
+# In[36]:
+
+
+a>=b
+
+
+# In[37]:
+
+
+b=10
+a>=b
+
+
+# In[38]:
+
+
+a+=b
+a
+
+
+# In[40]:
+
+
+a-=b
+a
+
+
+# In[43]:
+
+
+a**=b
+a
+
+
+# In[44]:
+
+
+str="PythonProgramming"
+'p' in str
+
+
+# In[45]:
+
+
+
+p=12
+q=12
+p is q
+
+
+# In[ ]:
+
+
+
+
