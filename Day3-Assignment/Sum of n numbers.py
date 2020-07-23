ff79f99b8b1e231018be7f89d10f7315c9e9821a
teju0809 <46315769+teju0809@users.noreply.github.com>
2020-07-24 00:48:41 +0530

    Add files via upload

---
+++ Day3-Assignment/Sum of n numbers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Sum of N numbers
+
+# In[3]:
+
+
+num =int(input("Enter the number"))
+i= 0
+while( num > 0):
+    i=i+num
+    num=num-1
+print("The sum of N numbers is:",i)   
+
+
+# In[ ]:
+
+
+
+
